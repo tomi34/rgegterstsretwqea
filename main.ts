adaa6d3fb80bb22ef447dd3073518800a01c1f9e
tomi34 <66636695+tomi34@users.noreply.github.com>
2022-12-15 12:34:31 +0100

    pxt.json, main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -1,3 +1,67 @@
-basic.forever(function () {
-	
+function saf () {
+    if (0 == zd) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+        basic.pause(8000)
+        if (0 == zd) {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . # . # .
+                # # . # #
+                `)
+        }
+        basic.pause(6000)
+        if (0 == zd) {
+            for (let index = 0; index < 3; index++) {
+                if (0 == zd) {
+                    basic.clearScreen()
+                    basic.pause(500)
+                    basic.showLeds(`
+                        . . # . .
+                        . . # . .
+                        . . # . .
+                        . # . # .
+                        # # . # #
+                        `)
+                    basic.pause(500)
+                }
+            }
+            saf()
+        }
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    zd += 1
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . # . # .
+        # # . # #
+        `)
+    basic.pause(6000)
+    for (let index = 0; index < 3; index++) {
+        basic.clearScreen()
+        basic.pause(500)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . # . # .
+            # # . # #
+            `)
+        basic.pause(500)
+    }
+    zd = 0
+    saf()
 })
+let zd = 0
+zd = 0
+saf()
